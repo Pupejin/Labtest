478c0c6c53e2ed233c3b4a32f6e16a2bee10e5b0
Unchana Daking <poopeunchana@gmail.com>
2016-12-12 22:55:27 +0800

    add newnew

---
+++ src/main/java/co/sping/domain/Newnew.java
@@ -0,0 +1,15 @@
+package co.sping.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import java.lang.Folat;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Newnew {
+
+    /**
+     */
+    private Folat num;
+}
