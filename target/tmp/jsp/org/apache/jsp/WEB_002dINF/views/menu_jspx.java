9f849cf84d4a99fb2debfdb397246d24bf485f3c
Unchana Daking <poopeunchana@gmail.com>
2016-12-12 22:45:32 +0800

    add ITT

--- target/tmp/jsp/org/apache/jsp/WEB_002dINF/views/menu_jspx.java
+++ target/tmp/jsp/org/apache/jsp/WEB_002dINF/views/menu_jspx.java
@@ -11,6 +11,13 @@ public final class menu_jspx extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
+  static {
+    _jspx_dependants = new java.util.ArrayList<String>(3);
+    _jspx_dependants.add("/WEB-INF/tags/menu/menu.tagx");
+    _jspx_dependants.add("/WEB-INF/tags/menu/category.tagx");
+    _jspx_dependants.add("/WEB-INF/tags/menu/item.tagx");
+  }
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
@@ -42,6 +49,8 @@ public final class menu_jspx extends org.apache.jasper.runtime.HttpJspBase
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
       out.write("<div version=\"2.0\" id=\"menu\">");
+      if (_jspx_meth_menu_menu_0(_jspx_page_context))
+        return;
       out.write("</div>");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
@@ -53,6 +62,126 @@ public final class menu_jspx extends org.apache.jasper.runtime.HttpJspBase
       }
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_menu_menu_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  menu:menu
+    org.apache.jsp.tag.web.menu.menu_tagx _jspx_th_menu_menu_0 = (_jspx_resourceInjector != null) ? _jspx_resourceInjector.createTagHandlerInstance(org.apache.jsp.tag.web.menu.menu_tagx.class) : new org.apache.jsp.tag.web.menu.menu_tagx();
+    _jspx_th_menu_menu_0.setJspContext(_jspx_page_context);
+    _jspx_th_menu_menu_0.setZ("nZaf43BjUg1iM0v70HJVEsXDopc=");
+    _jspx_th_menu_menu_0.setId("_menu");
+    _jspx_th_menu_menu_0.setJspBody(new menu_jspxHelper( 0, _jspx_page_context, _jspx_th_menu_menu_0, null));
+    _jspx_th_menu_menu_0.doTag();
+    return false;
+  }
+
+  private boolean _jspx_meth_menu_category_0(javax.servlet.jsp.tagext.JspTag _jspx_parent, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  menu:category
+    org.apache.jsp.tag.web.menu.category_tagx _jspx_th_menu_category_0 = (_jspx_resourceInjector != null) ? _jspx_resourceInjector.createTagHandlerInstance(org.apache.jsp.tag.web.menu.category_tagx.class) : new org.apache.jsp.tag.web.menu.category_tagx();
+    _jspx_th_menu_category_0.setJspContext(_jspx_page_context);
+    _jspx_th_menu_category_0.setParent(_jspx_parent);
+    _jspx_th_menu_category_0.setZ("VseHQbIoDR931YBA8W2zFL4RKlQ=");
+    _jspx_th_menu_category_0.setId("c_inform");
+    _jspx_th_menu_category_0.setJspBody(new menu_jspxHelper( 1, _jspx_page_context, _jspx_th_menu_category_0, null));
+    _jspx_th_menu_category_0.doTag();
+    return false;
+  }
+
+  private boolean _jspx_meth_menu_item_0(javax.servlet.jsp.tagext.JspTag _jspx_parent, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  menu:item
+    org.apache.jsp.tag.web.menu.item_tagx _jspx_th_menu_item_0 = (_jspx_resourceInjector != null) ? _jspx_resourceInjector.createTagHandlerInstance(org.apache.jsp.tag.web.menu.item_tagx.class) : new org.apache.jsp.tag.web.menu.item_tagx();
+    _jspx_th_menu_item_0.setJspContext(_jspx_page_context);
+    _jspx_th_menu_item_0.setParent(_jspx_parent);
+    _jspx_th_menu_item_0.setZ("q0W0GOGvmyXfJU7STKzN4NZiwQI=");
+    _jspx_th_menu_item_0.setUrl("/informs?form");
+    _jspx_th_menu_item_0.setMessageCode("global_menu_new");
+    _jspx_th_menu_item_0.setId("i_inform_new");
+    _jspx_th_menu_item_0.doTag();
+    return false;
+  }
+
+  private boolean _jspx_meth_menu_item_1(javax.servlet.jsp.tagext.JspTag _jspx_parent, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  menu:item
+    org.apache.jsp.tag.web.menu.item_tagx _jspx_th_menu_item_1 = (_jspx_resourceInjector != null) ? _jspx_resourceInjector.createTagHandlerInstance(org.apache.jsp.tag.web.menu.item_tagx.class) : new org.apache.jsp.tag.web.menu.item_tagx();
+    _jspx_th_menu_item_1.setJspContext(_jspx_page_context);
+    _jspx_th_menu_item_1.setParent(_jspx_parent);
+    _jspx_th_menu_item_1.setZ("xs3EPg6N+rsikC16CGqPEmq1P54=");
+    _jspx_th_menu_item_1.setUrl((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("/informs?page=1&size=${empty param.size ? 10 : param.size}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+    _jspx_th_menu_item_1.setMessageCode("global_menu_list");
+    _jspx_th_menu_item_1.setId("i_inform_list");
+    _jspx_th_menu_item_1.doTag();
+    return false;
+  }
+
+  private class menu_jspxHelper
+      extends org.apache.jasper.runtime.JspFragmentHelper
+  {
+    private javax.servlet.jsp.tagext.JspTag _jspx_parent;
+    private int[] _jspx_push_body_count;
+
+    public menu_jspxHelper( int discriminator, JspContext jspContext, javax.servlet.jsp.tagext.JspTag _jspx_parent, int[] _jspx_push_body_count ) {
+      super( discriminator, jspContext, _jspx_parent );
+      this._jspx_parent = _jspx_parent;
+      this._jspx_push_body_count = _jspx_push_body_count;
+    }
+    public boolean invoke0( JspWriter out ) 
+      throws Throwable
+    {
+      if (_jspx_meth_menu_category_0((javax.servlet.jsp.tagext.JspTag) _jspx_parent, _jspx_page_context))
+        return true;
+      return false;
+    }
+    public boolean invoke1( JspWriter out ) 
+      throws Throwable
+    {
+      if (_jspx_meth_menu_item_0((javax.servlet.jsp.tagext.JspTag) _jspx_parent, _jspx_page_context))
+        return true;
+      if (_jspx_meth_menu_item_1((javax.servlet.jsp.tagext.JspTag) _jspx_parent, _jspx_page_context))
+        return true;
+      return false;
+    }
+    public void invoke( java.io.Writer writer )
+      throws JspException
+    {
+      JspWriter out = null;
+      if( writer != null ) {
+        out = this.jspContext.pushBody(writer);
+      } else {
+        out = this.jspContext.getOut();
+      }
+      try {
+        switch( this.discriminator ) {
+          case 0:
+            invoke0( out );
+            break;
+          case 1:
+            invoke1( out );
+            break;
+        }
+      }
+      catch( Throwable e ) {
+        if (e instanceof SkipPageException)
+            throw (SkipPageException) e;
+        throw new JspException( e );
+      }
+      finally {
+        if( writer != null ) {
+          this.jspContext.popBody();
+        }
+      }
     }
   }
 }
